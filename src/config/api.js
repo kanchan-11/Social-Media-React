9df82f6111b73ca7b30dbdb8b901ec53e835d913
kanchan-11 <agarwallkanchan29@gmail.com>
2024-03-09 19:51:26 +0530

    connecting production backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const jwtToken=localStorage.getItem("jwt")
-export const API_BASE_URL="http://localhost:5454";
+// export const API_BASE_URL="http://localhost:5454";
+export const API_BASE_URL="https://social-media-application-production.up.railway.app";
 export const api =axios.create({baseURL:API_BASE_URL,
         headers:{
             "Authorization":`Bearer ${jwtToken}`,
